5d4f5114579131be983d03f53a3dc07dbd6e7ec8
sankari2005-art <sankarisreemangalam@gmail.com>
2026-02-20 23:03:46 +0530

    Update train_model.py

--- training/train_model.py
+++ training/train_model.py
@@ -1,1 +1,11 @@
-!python train.py --img 640 --batch 16 --epochs 1 --data coco128.yaml --weights yolov5s.pt
+python train.py \
+--img 640 \
+--batch 16 \
+--epochs 50 \
+--data dataset.yaml \
+--weights yolov5n.pt \
+--name congestion_model
+
+python export.py \
+--weights runs/train/congestion_model/weights/best.pt \
+--include onnx
